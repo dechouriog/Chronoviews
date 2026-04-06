3b061efd15e728b2ca5128d0e4f675316d3f1b82
DCH <141684387+dechouriog@users.noreply.github.com>
2026-04-06 19:33:07 +0000

    Agregando Seeder con usuarios normal y admin

---
+++ frontend/src/stores/authseeder.ts
@@ -0,0 +1,22 @@
+import type { UserInterface } from '@/interfaces/UserInterface';
+
+export const authSeeder: UserInterface[] = [
+  {
+    id: '1',
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'admin123',
+    role: 'admin',
+    totalTrackedTime: 0,
+    tasksCount: 0,
+  },
+  {
+    id: '2',
+    name: 'Standard User',
+    email: 'user@example.com',
+    password: 'user123',
+    role: 'user',
+    totalTrackedTime: 0,
+    tasksCount: 0,
+  },
+];
