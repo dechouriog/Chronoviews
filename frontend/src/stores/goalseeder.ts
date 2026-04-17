70cdcc2de6cc0fd2f8dd7fa9911e20c67fe3b25b
DCH <141684387+dechouriog@users.noreply.github.com>
2026-04-17 15:54:53 +0000

    Update: GoalSeeder

--- frontend/src/stores/goalseeder.ts
+++ frontend/src/stores/goalseeder.ts
@@ -1,8 +1,8 @@
 import type { GoalInterface } from '@/interfaces/GoalInterface';
 
 export const goalSeeder: GoalInterface[] = [
-  { id: '1', userId: '1', taskId: '1', targetHours: 40, currentHours: 2, period: 'weekly' },
-  { id: '2', userId: '1', taskId: '2', targetHours: 10, currentHours: 1, period: 'weekly' },
-  { id: '3', userId: '1', taskId: '3', targetHours: 5, currentHours: 0.5, period: 'weekly' },
-  { id: '4', userId: '1', taskId: '4', targetHours: 8, currentHours: 0.75, period: 'weekly' },
+  { id: '1', userId: '1', taskId: '1', targetHours: 40,period: 'monthly' },
+  { id: '2', userId: '1', taskId: '2', targetHours: 10,period: 'monthly' },
+  { id: '3', userId: '1', taskId: '3', targetHours: 5, period: 'monthly' },
+  { id: '4', userId: '1', taskId: '4', targetHours: 8, period: 'monthly' },
 ];
