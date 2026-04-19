71828ca18741980a63ffac3ff23fd8f252274deb
Leidy Obando <125570525+caro0616@users.noreply.github.com>
2026-04-19 15:46:19 +0000

    Create: User.controller

---
+++ backend/src/users/Users.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { User } from './entities/user.entity.js';
+import { UsersService } from './users.service.js';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findOne(id);
+  }
+}
